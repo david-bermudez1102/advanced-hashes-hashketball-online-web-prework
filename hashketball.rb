6ae1d7f5763ab5fa234a01e15d4f173d66bfefab
Juan Bermudez <juan.bermudez1102@outlook.com>
2019-09-29 20:20:59 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -231,7 +231,7 @@ end
 def long_name_steals_a_ton?
   steals = Array.new
   player = player_with_longest_name
-  
+
   game_hash.each_with_index { |(team,level), index|
     level[:players].each { |stat|
         steals[index] = stat[:steals]
