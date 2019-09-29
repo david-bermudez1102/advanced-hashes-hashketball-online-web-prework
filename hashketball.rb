7482a8c8368c12f25d6a942908f9ada4fea33db8
Juan Bermudez <juan.bermudez1102@outlook.com>
2019-09-29 19:12:28 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -139,6 +139,8 @@ end
 
 def big_shoe_rebounds
   shoe_sizes = Array.new
+  number_of_rebounds = 0
+  
   game_hash.each_with_index { |(team,level), index|
     level[:players].each { |stat|
         shoe_sizes[index] = stat[:shoe].map(&:to_i)
@@ -152,7 +154,7 @@ def big_shoe_rebounds
   game_hash.each { |team,level|
     level[:players].each { |stat|
       index = (stat[:shoe_size]).find_index(biggest_shoe_size.to_s)
-
+      
     }
   }
 biggest_shoe_size
